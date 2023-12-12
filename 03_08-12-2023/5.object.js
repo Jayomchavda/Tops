db497893b9671df99f358f45dd1351cf12854674
jayom <jayomchavda4@gmail.com>
2023-12-12 22:09:43 +0530

    12change

--- 03_08-12-2023/5.object.js
+++ 03_08-12-2023/5.object.js
@@ -93,6 +93,53 @@ console.log("All keys:", allKeys);
 
 // =================================================================
 
-// 
+// get all values
+/*
+function createobj(name, age, vehicles) {
+    return {
+        name: name,
+        age: age,
+        vehicles: vehicles,
+        printName: function () {
+            console.log("My name is", this.name);
+        },
+        printAge: function () {
+            console.log("My age is", this.age);
+        },
+        getAllKeys: function () {
+            return Object.keys(this);
+        },
+        getAllValues: function () {
+            return Object.values(this);
+        }
+    };
+}
+
+let myobj = createobj("jayom", 20, ["Car", "Bicycle"]);
+
+console.log("Name:", myobj.name);
+console.log("Age:", myobj.age);
+console.log("Vehicles:", myobj.vehicles);
+
+myobj.printName();
+myobj.printAge();
+
+let allKeys = myobj.getAllKeys();
+console.log("All keys:", allKeys);
+
+let allValues = myobj.getAllValues();
+console.log("All values:", allValues);
+
+*/
+
+// =================================================================
+
+// add one key for city name and store your city name on it  (from outside)
+
+
+
+
+
+
 
 
