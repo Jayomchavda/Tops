47b431dd51d9af60cd8fa0d286995b754cb81a89
jayom <jayomchavda4@gmail.com>
2024-01-04 22:33:54 +0530

    04change

--- React/12-22/jayom/src/App.jsx
+++ React/12-22/jayom/src/App.jsx
@@ -1,23 +1,47 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+import ClassCom from './1.Component/class/ClassCom.jsx';
+import FunctionalCom from './2.functional/FunctionalCom.jsx';
+import ClassCom2 from './1.Component/ClassCom2.jsx';
+import Home from './3.Style/Home.jsx';
+import Home2 from './4.TaskDesign/Home2.jsx';
+import State from './5.State/State.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Props from './6.Props/Functional/props.jsx';
+
 
 
 export default function App() {
   return (
     <div>
-      <functional />
+      {/* ==================   Header,Body ,Footer TASK ================= */}
+
+      {/* <ClassCom />
+    <FunctionalCom />
+    <ClassCom2 /> */}
+
+
+      {/* =====================  3.Style  ==================== */}
+
+      {/* <Home /> */}
+
+      {/* =====================  4.TaskDesign  ==================== */}
+
+      {/* <Home2 /> */}
+
+      {/* =====================  5.State  ==================== */}
+
+      {/* <State /> */}
+
+      {/* =====================  6.Props  ==================== */}
+
+      <Props />
+
     </div>
   )
 }
 
 
-// import React, { Component } from 'react'
 
-// export default class ClassCom extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <h1>Hello User...!</h1>
-//         <h2>My name is Jayom </h2>
-//       </div>
-//     )
-//   }
-// }
--- React/12-22/jayom/src/main.jsx
+++ React/12-22/jayom/src/main.jsx
@@ -1,55 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-import ClassCom from './1.Component/class/ClassCom.jsx';
-import ClassCom2 from './1.Component/ClassCom2.jsx';
-import FunctionalCom from './2.functional/FunctionalCom.jsx';
-import Home from './3.Style/Home.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Home2 from './4.TaskDesign/Home2.jsx';
-import Funstate from './5.State/Functional/Funstate.jsx';
-import State from './5.State/State.jsx';
-import StatePro1 from './5.State/Functional/StatePro1.jsx';
-import Props from './6.Props/Functional/props.jsx';
-import ClassProps from './6.Props/Functional/Class/ClassProps.jsx';
+import App from './App'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* ==================   Header,Body ,Footer TASK ================= */}
-    {/* <ClassCom />
-    <FunctionalCom />
-    <ClassCom2 /> */}
-
-    {/* <App /> */}
-
-    {/* =====================  3.Style  ==================== */}
-
-    {/* <Home /> */}
-
-    {/* =====================  4.TaskDesign  ==================== */}
-
-    {/* <Home2 /> */}
-
-    {/* =====================  5.State  ==================== */}
-
-    {/* <State /> */}
-
-    {/* =====================  6.Props  ==================== */}
-
-    <Props />
-    {/* <ClassProps /> */}
-
-
-
-
-
-
-
-
-
-
-
-
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 )
