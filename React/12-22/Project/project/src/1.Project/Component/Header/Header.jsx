8df465f6118c96edcb6e1c57164aaff83eac0248
jayom <jayomchavda4@gmail.com>
2024-03-10 15:14:48 +0530

    adddata

---
+++ React/12-22/Project/project/src/1.Project/Component/Header/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import "./Header.css"
+import { Contact, Home, MessageCircle, Phone, Receipt, ReceiptCentIcon, Search, User } from 'lucide-react'
+import { Input } from 'reactstrap'
+
+export default function Header() {
+    return (
+
+        <div className='headercom'>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRW2Su_acuNksn_4ga4YgMA6aDUBNRI6YuEYA&usqp=CAU" style={{ width: "60px", borderRadius: "50px", marginLeft: "30px" }} />
+            <h1 style={{ marginLeft: "10px" }}>Portfolio</h1>
+
+            <div className='navlink'>
+                <NavLink to={"/"}><Home /> Home</NavLink >
+                <NavLink to={"/about"}> <MessageCircle /> About</NavLink >
+                <NavLink to={"/resume"}><User /> Resume</NavLink >
+                <NavLink to={"/contact"}><Phone /> Contact</NavLink >
+                <div>
+                    <Input placeholder='  Search...' style={{ borderRadius: "50px", width: "200px", height: "30px", }} />
+                    <Search role='button' style={{ border: "none" }} />
+                </div>
+
+            </div>
+
+        </div>
+
+    )
+}
---
+++ React/12-22/Project/project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Router from './1.Project/Router/Router'
+
+
+
+export default function App() {
+  return (
+    <div>
+      <Router />
+    </div>
+  )
+}
